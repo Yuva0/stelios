1454e75f3be879bd7b337ad5aec667d9b1d795c6
Tanuj Sengupta <senguptatanuj@gmail.com>
2024-08-06 19:45:05 +0530

    List prop updated

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -11,7 +11,7 @@ const StyledList = styled.ul<ListStyleProps>`
   display: flex;
   flex-direction: column;
   gap: 0.5rem;
-  margin: 0;
+  margin: 0.75rem 0 0 0;
   padding: 0 0 0 2rem;
   font-family: "Lato", sans-serif;
   list-style-type: ${(props) =>
