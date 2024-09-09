eb653c0722d36af394c1c4a0c5c98e81189c60f8
Tanuj Sengupta <senguptatanuj@gmail.com>
2024-09-09 11:14:07 +0530

    Updated navigation bar storybook

--- src/stories/components/NavigationBar/NavigationBar.stories.tsx
+++ src/stories/components/NavigationBar/NavigationBar.stories.tsx
@@ -23,76 +23,56 @@ type Story = StoryObj<typeof NavigationBar>;
 const Template: Story = {
   render: () => {
     return (
-      <ThemeProvider accents={{ primary: "red" }}>
-        <NavigationBar color="primary" onChange={(value) => console.log(value)}>
-          <NavigationBarGroup title="Group 1">
-            <NavigationBarHeader>Header 1</NavigationBarHeader>
-            <NavigationBarGroupItem value="1">
-              Group Items
-            </NavigationBarGroupItem>
-            <NavigationBarGroupItem value="2">
-              Group Items
-            </NavigationBarGroupItem>
-            <NavigationBarGroupItem value="3">
-              Group Items
-            </NavigationBarGroupItem>
-            <NavigationBarGroupItem value="4">
-              Group Items
-            </NavigationBarGroupItem>
-            <NavigationBarGroupItem value="5">
-              Group Items
-            </NavigationBarGroupItem>
-            <NavigationBarHeader>Header 2</NavigationBarHeader>
-            <NavigationBarGroupItem value="6">
-              Group Items
-            </NavigationBarGroupItem>
-            <NavigationBarGroupItem value="7">
-              Group Items
-            </NavigationBarGroupItem>
-            <NavigationBarGroupItem value="8">
-              Group Items
-            </NavigationBarGroupItem>
-            <NavigationBarGroupItem value="9">
-              Group Items
-            </NavigationBarGroupItem>
-          </NavigationBarGroup>
-          <NavigationBarGroup title="Group 2">
-            <NavigationBarHeader>Header 3</NavigationBarHeader>
-            <NavigationBarGroupItem value="13">
-              Group Items
-            </NavigationBarGroupItem>
-            <NavigationBarGroupItem value="14">
-              Group Items
-            </NavigationBarGroupItem>
-            <NavigationBarGroupItem value="15">
-              Group Items
-            </NavigationBarGroupItem>
-            <NavigationBarGroupItem value="16">
-              Group Items
-            </NavigationBarGroupItem>
-            <NavigationBarGroupItem value="17">
-              Group Items
-            </NavigationBarGroupItem>
-            <NavigationBarGroupItem value="18">
-              Group Items
-            </NavigationBarGroupItem>
-            <NavigationBarGroupItem value="19">
-              Group Items
-            </NavigationBarGroupItem>
-            <NavigationBarGroupItem value="20">
-              Group Items
-            </NavigationBarGroupItem>
-            <NavigationBarGroupItem value="21">
-              Group Items
-            </NavigationBarGroupItem>
-          </NavigationBarGroup>
-          <NavigationBarItem selected value="22">
-            Title Item
-          </NavigationBarItem>
-          <NavigationBarItem value="23">Title Item</NavigationBarItem>
-          <NavigationBarItem value="22">Title Item</NavigationBarItem>
-        </NavigationBar>
-      </ThemeProvider>
+      <NavigationBar color="primary" onChange={(value) => console.log(value)}>
+        <NavigationBarGroup title="Group 1">
+          <NavigationBarHeader>Header 1</NavigationBarHeader>
+          <NavigationBarGroupItem value="1">Group Items</NavigationBarGroupItem>
+          <NavigationBarGroupItem value="2">Group Items</NavigationBarGroupItem>
+          <NavigationBarGroupItem value="3">Group Items</NavigationBarGroupItem>
+          <NavigationBarGroupItem value="4">Group Items</NavigationBarGroupItem>
+          <NavigationBarGroupItem value="5">Group Items</NavigationBarGroupItem>
+          <NavigationBarHeader>Header 2</NavigationBarHeader>
+          <NavigationBarGroupItem value="6">Group Items</NavigationBarGroupItem>
+          <NavigationBarGroupItem value="7">Group Items</NavigationBarGroupItem>
+          <NavigationBarGroupItem value="8">Group Items</NavigationBarGroupItem>
+          <NavigationBarGroupItem value="9">Group Items</NavigationBarGroupItem>
+        </NavigationBarGroup>
+        <NavigationBarGroup title="Group 2">
+          <NavigationBarHeader>Header 3</NavigationBarHeader>
+          <NavigationBarGroupItem value="13">
+            Group Items
+          </NavigationBarGroupItem>
+          <NavigationBarGroupItem value="14">
+            Group Items
+          </NavigationBarGroupItem>
+          <NavigationBarGroupItem value="15">
+            Group Items
+          </NavigationBarGroupItem>
+          <NavigationBarGroupItem value="16">
+            Group Items
+          </NavigationBarGroupItem>
+          <NavigationBarGroupItem value="17">
+            Group Items
+          </NavigationBarGroupItem>
+          <NavigationBarGroupItem value="18">
+            Group Items
+          </NavigationBarGroupItem>
+          <NavigationBarGroupItem value="19">
+            Group Items
+          </NavigationBarGroupItem>
+          <NavigationBarGroupItem value="20">
+            Group Items
+          </NavigationBarGroupItem>
+          <NavigationBarGroupItem value="21">
+            Group Items
+          </NavigationBarGroupItem>
+        </NavigationBarGroup>
+        <NavigationBarItem selected value="22">
+          Title Item
+        </NavigationBarItem>
+        <NavigationBarItem value="23">Title Item</NavigationBarItem>
+        <NavigationBarItem value="22">Title Item</NavigationBarItem>
+      </NavigationBar>
     );
   },
 };
